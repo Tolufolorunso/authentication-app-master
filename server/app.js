0aa0b5b4e1dc1949fe73e12150128783d8a0336a
Tolu Folorunso <tolufolorunso@yahoo.com>
2022-07-20 19:59:46 +0100

    server set-up

--- server/app.js
+++ server/app.js
@@ -1,1 +1,21 @@
-console.log('hello');
+require('dotenv').config();
+require('express-async-errors');
+const express = require('express');
+const morgan = require('morgan');
+
+const { StatusCodes } = require('http-status-codes');
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.get('/', (req, res, next) => {
+  res.status(StatusCodes.OK).json({
+    status: true,
+    message: 'Authentication App by devchallenges.io',
+  });
+});
+
+module.exports = app;
